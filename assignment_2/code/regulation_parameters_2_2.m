d23ce4de5b06473010b02397e89c474dda2e6b9c
Ella-Lovise H. Rørvik <elh.rorvik@gmail.com>
2017-09-27 09:21:34 +0200

    skdjlfls

--- assignment_2/code/regulation_parameters_2_2.m
+++ assignment_2/code/regulation_parameters_2_2.m
@@ -18,6 +18,23 @@ k_i_phi = 1; %temp
 %k_p_chi = 2*zeta_chi*omega_n_chi*V_g/g;
 %k_i_chi = omega_n_chi^2*Vg/g;
 
+A = [ -0.322 0.052 0.028 -1.12 0.002;
+     0 0 1 -0.001 0;
+     -10.6 0 -2.87 0.46 -0.65;
+     6.87 0 -0.04 -0.32 -0.02;
+     0 0 0 0 -10];
+B = [ 0 0 0 0 10]';
+C = [0 0 0 1 0;
+     0 0 1 0 0;
+     1 0 0 0 0 ;
+     0 1 0 0 0];
+D = [0 0 0 0]';
+
+sys = ss(A,B,C,D);
+Phi = inv(s*eye(5)-A);
+trans = C*Phi*B + D;
+
+
 s = tf('s');
 plant = -0.65/(s - 2.)*(omega_n_phi)^2/(s^2 + 2*zeta_phi*omega_n_phi*s + omega_n_phi^2);
 
@@ -31,4 +48,9 @@ H_evan = 1 + k_i_phi*(a_phi_2/(s*(s^2+(a_phi_1 + a_phi_2*k_d_phi)*s + a_phi_2*k_
 
 H_evCar = s*(s^2+(a_phi_1 + a_phi_2*k_d_phi)*s + a_phi_2*k_p_phi);
 
-controlSystemDesigner('rlocus',H_evCar);
+%controlSystemDesigner('rlocus',H_evCar);
+
+syms beta r 
+
+eqn1 = A_11*
+
