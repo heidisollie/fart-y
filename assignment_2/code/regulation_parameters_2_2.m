609ff91e8cbd224db5e91de45c1102d90372b02e
allinox <arnex04@gmail.com>
2017-09-27 18:58:51 +0200

    On 2d

--- assignment_2/code/regulation_parameters_2_2.m
+++ assignment_2/code/regulation_parameters_2_2.m
@@ -1,6 +1,11 @@
+close all;
+clear all;
+clc;
+
 % some constants
 deg2rad = pi/180;   
 rad2deg = 180/pi;
+g = 9.81;
 
 V_g = 637*3.6; % m/s
 delta_a_max = 25*rad2deg;
@@ -38,17 +43,70 @@ trans = C*Phi*B + D;
 
 plant = -0.65/(s - 2.)*(omega_n_phi)^2/(s^2 + 2*zeta_phi*omega_n_phi*s + omega_n_phi^2);
 
-H_el = (a_phi_2*(k_i_phi/s + k_p_phi))/((1+k_d_phi*(a_phi_2/(s+a_phi_1)))*(s+a_phi_1)*s+a_phi_2*(k_i_phi/s+k_p_phi));
-
-H_bok = (a_phi_2*k_p_phi*(s+(k_i_phi + k_p_phi)))/(s^3 + (a_phi_1 + a_phi_2*k_d_phi)*s^2 + a_phi_2*k_p_phi*s + a_phi_2*k_i_phi);
-
-H_car = (s^3 + (a_phi_1 + a_phi_2*k_d_phi)*s^2 + a_phi_2*k_p_phi*s + a_phi_2*k_i_phi);
-
-H_evan = 1 + k_i_phi*(a_phi_2/(s*(s^2+(a_phi_1 + a_phi_2*k_d_phi)*s + a_phi_2*k_p_phi)));
-
 H_evCar = s*(s^2+(a_phi_1 + a_phi_2*k_d_phi)*s + a_phi_2*k_p_phi);
 
-controlSystemDesigner('rlocus',H_evCar);
+%controlSystemDesigner('rlocus',1/H_evCar);
+
+W_chi = 10;
+zeta_chi = 1;
+
+omega_n_chi = 1/W_chi*omega_n_phi;
+
+H_chi = (2*zeta_chi*omega_n_chi*s + omega_n_chi^2)/(s^2 + 2*zeta_chi*omega_n_chi*s + omega_n_chi^2);
+
+%controlSystemDesigner('rlocus',H_chi);
+
+k_p_chi = 2*zeta_chi*omega_n_chi*V_g/g;
+
+k_i_chi = omega_n_chi^2*V_g/g;
+
+%% Task 2d
+
+H_phi = a_phi_2*(k_p_phi*s + k_i_phi)/(s^2*(s + a_phi_1 + a_phi_2*k_d_phi));
+
+H_chi = g/V_g*(a_phi_2*(k_p_phi*s + k_i_phi)*(k_i_chi+k_p_chi*s))/(s^4*(s+a_phi_1 + a_phi_2*k_d_phi));
+
+figNum = 1;
+figure(figNum)
+bode(H_phi); hold on; grid on;
+bode(H_chi); legend('H_{\phi}','H_{\chi}');
+
+figNum = figNum +1;
+figure(figNum)
+margin(H_phi);grid on; legend('H_{\phi}');
+
+figNum = figNum +1;
+figure(figNum)
+margin(H_chi);grid on; legend('H_{\chi}');
+
+% Changing the freq
+
+omega_n_chi = omega_n_phi;
+
+k_p_chi = 2*zeta_chi*omega_n_chi*V_g/g;
+
+k_i_chi = omega_n_chi^2*V_g/g;
+
+H_phi = a_phi_2*(k_p_phi*s + k_i_phi)/(s^2*(s + a_phi_1 + a_phi_2*k_d_phi));
+
+H_chi = g/V_g*(a_phi_2*(k_p_phi*s + k_i_phi)*(k_i_chi+k_p_chi*s))/(s^4*(s+a_phi_1 + a_phi_2*k_d_phi));
+
+figNum = figNum +1;
+figure(figNum)
+bode(H_phi); hold on; grid on;
+bode(H_chi); legend('H_{\phi}','H_{\chi}');
+
+figNum = figNum +1;
+figure(figNum)
+margin(H_phi);grid on; legend('H_{\phi}');
+
+figNum = figNum +1;
+figure(figNum)
+margin(H_chi);grid on; legend('H_{\chi}');
+
+
+
+
 
 
 
