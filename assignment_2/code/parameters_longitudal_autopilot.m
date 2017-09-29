5c8e4dbbdf8ffef1ce4475d5cb7ed6d187597f52
allinox <arnex04@gmail.com>
2017-09-29 12:25:27 +0200

    add parameters

--- assignment_2/code/parameters_longitudal_autopilot.m
+++ assignment_2/code/parameters_longitudal_autopilot.m
@@ -3,19 +3,19 @@ deg2rad = pi/180;
 rad2deg = 180/pi;
 g = 9.81;
 
-V_g = 637*3.6; % m/s
+V_g = 637/3.6; % m/s
 Tl = 1/10;
 delta_a_max = 25*deg2rad;
 e_phi_max = 15*deg2rad;
 zeta_phi = 0.707;
-a_phi_1 = 36.67; %2.87;
-a_phi_2 = 211.35; %-0.65;
+a_phi_1 = 2.87; 
+a_phi_2 = -0.65; 
 
 omega_n_phi = sqrt(abs(a_phi_2)*delta_a_max /e_phi_max);
 
-k_p_phi = delta_a_max /e_phi_max; %both a is positive
+k_p_phi = delta_a_max /e_phi_max*sign(a_phi_2);
 k_d_phi = (2*zeta_phi*omega_n_phi - a_phi_1) / a_phi_2;
-k_i_phi = 1; %temp
+k_i_phi = -1; %temp
 
 W_chi = 7;
 zeta_chi = 1;
